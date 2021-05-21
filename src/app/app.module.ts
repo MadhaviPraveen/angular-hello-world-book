4f4fbda5bfa113c0b6d2824b5f8ecb5fe7dc5748
Madhavi Ivaturi <Madhavi.Ivaturi@freschelegacy.com>
2021-05-21 06:15:09 -0400

    Components-helloWorld, userItem are created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
